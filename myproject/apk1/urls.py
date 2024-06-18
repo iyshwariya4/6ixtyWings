8b37daabed128de51ac8eb283b73df12b7ed8ba1
jk <130308192+dilshanjohnjk@users.noreply.github.com>
2024-06-18 13:48:42 +0530

    Add files via upload
    initial commoit by jk

---
+++ myproject/apk1/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('main.html', views.say_hello),
+    path('register.html',views.register, name='register'),
+    path('collections.html',views.collections, name='collections'),
+    path('collections/<str:slug>',views.collectionsview, name='collectionsview')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
